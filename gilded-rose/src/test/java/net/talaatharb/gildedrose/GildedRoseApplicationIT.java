322d6983c58f18f1e18091949d01ad9a36ee03de
mharb <mohamed.harb@cegedim.com>
2024-12-05 16:17:42 +0200

    added starting point for gilded-rose

---
+++ gilded-rose/src/test/java/net/talaatharb/gildedrose/GildedRoseApplicationIT.java
@@ -0,0 +1,15 @@
+package net.talaatharb.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GildedRoseApplicationIT {
+
+	@Test
+	void testApplicationRuns() {
+		GildedRoseApplication.main(new String[] {});
+		assertTrue(true);
+	}
+
+}
