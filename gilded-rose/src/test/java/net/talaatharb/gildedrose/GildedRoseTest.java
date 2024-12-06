99957074ed9d8c18575804b0e606fa08903b75ae
mharb <mohamed.harb@cegedim.com>
2024-12-06 10:34:09 +0200

    added approval tests to cover functionality

--- gilded-rose/src/test/java/net/talaatharb/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/net/talaatharb/gildedrose/GildedRoseTest.java
@@ -1,17 +1,25 @@
 package net.talaatharb.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
+import org.lambda.utils.Range;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	void approvalTests() {
+		var names = new String[] { "foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+				"Sulfuras, Hand of Ragnaros" };
+		var sellIns = Range.get(-1, 15);
+		var qualities = new Integer[] { 0, -1, 1, 49, 50, 51 };
+		CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, names, sellIns, qualities);
+	}
+
+	private String doUpdateQuality(String name, int sellIn, int quality) {
+		Item[] items = new Item[] { new Item(name, sellIn, quality) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		return app.items[quality].toString();
+	}
 
 }
