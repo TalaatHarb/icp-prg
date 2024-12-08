424783a4f761372386c1b8dd8a228c4caf055d48
mharb <mohamed.harb@cegedim.com>
2024-12-08 15:23:51 +0200

    added tests and implementation for roman-numerals

--- roman-numerals/src/main/java/net/talaatharb/romannumerals/RomanNumeralDisplay.java
+++ roman-numerals/src/main/java/net/talaatharb/romannumerals/RomanNumeralDisplay.java
@@ -2,8 +2,20 @@ package net.talaatharb.romannumerals;
 
 public class RomanNumeralDisplay {
 
+	private static final int[] ARABIC_NUMBERS = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	private static final String[] ROMAN_NUMERALS = new String[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+			"V", "IV", "I" };
+
 	public String convertToRomanNumeral(int arabicNumber) {
-		// TODO implement correct conversion code
-		return "";
+		StringBuilder resultBuilder = new StringBuilder();
+
+		for (int i = 0; i < ARABIC_NUMBERS.length; i++) {
+			while (arabicNumber >= ARABIC_NUMBERS[i]) {
+				resultBuilder.append(ROMAN_NUMERALS[i]);
+				arabicNumber -= ARABIC_NUMBERS[i];
+			}
+		}
+
+		return resultBuilder.toString();
 	}
 }
--- roman-numerals/src/test/java/net/talaatharb/romannumerals/RomanNumeralDisplayTest.java
+++ roman-numerals/src/test/java/net/talaatharb/romannumerals/RomanNumeralDisplayTest.java
@@ -1,9 +1,10 @@
 package net.talaatharb.romannumerals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RomanNumeralDisplayTest {
 	
@@ -13,10 +14,27 @@ class RomanNumeralDisplayTest {
 	void setup() {
 		romanNumeralDisplay = new RomanNumeralDisplay();
 	}
-
-	@Test
-	void testConvert() {
-		assertTrue(true);
+	
+	@ParameterizedTest(name = "Should convert {0} to {1}")
+	@CsvSource(value = {
+			"1, I",
+			"2, II",
+			"3, III",
+			"4, IV",
+			"5, V",
+			"7, VII",
+			"9, IX",
+			"1990, MCMXC", 
+			"2008, MMVIII", 
+			"99, XCIX", 
+			"47, XLVII", 
+	})
+	void testAcceptanceOfMultipleExamples(final int arabicNumber, final String expectedResult ) {		
+		// Act
+		final String result = romanNumeralDisplay.convertToRomanNumeral(arabicNumber);
+		
+		// Assert
+		assertEquals(expectedResult, result);
 	}
 
 }
